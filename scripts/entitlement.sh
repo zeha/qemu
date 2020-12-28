2658eb36085e0fc94d586d68deb15bad6f1d7a63
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2020-12-28 13:17:15 +0100

    debug codesign

--- scripts/entitlement.sh
+++ scripts/entitlement.sh
@@ -2,6 +2,9 @@
 #
 # Helper script for the build process to apply entitlements
 
+echo "XXX entitlement.sh $@"
+file "$1"
+
 SRC="$1"
 DST="$2"
 ENTITLEMENT="$3"
@@ -11,3 +14,7 @@ cp -af "$SRC" "$DST.tmp"
 codesign --entitlements "$ENTITLEMENT" --force -s - "$DST.tmp"
 mv "$DST.tmp" "$DST"
 trap '' exit
+
+echo "XXXBEGIN CODESIGN STATUS FOR $DST"
+codesign -v -v -r- "$DST"
+echo "XXXEND CODESIGN STATUS FOR $DST"
